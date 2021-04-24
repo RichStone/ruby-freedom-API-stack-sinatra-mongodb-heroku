92015afcb42f8bb8eaef8a771a187142bd857283
RichStone <richard.had@hotmail.de>
2021-04-24 13:17:05 +0200

    Add mongoid to app

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@
 
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'mongoid'
+project_root = File.dirname(__FILE__)
+Mongoid.load!(File.join(project_root, 'mongoid.yml'))
 
 get '/freedom' do
   '“Freedom is not worth having if it does not include the freedom to make' \
