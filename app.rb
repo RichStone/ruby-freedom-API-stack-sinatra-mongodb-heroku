e5069527341f442e28697a4a5124c85b973ec4fa
RichStone <richard.had@hotmail.de>
2021-04-24 14:49:42 +0200

    Add mongoid model

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ require 'mongoid'
 project_root = File.dirname(__FILE__)
 Mongoid.load!(File.join(project_root, 'mongoid.yml'))
 
+class FreedomQuote
+  include Mongoid::Document
+
+  field :quote, type: String
+end
+
 get '/freedom' do
   '“Freedom is not worth having if it does not include the freedom to make' \
   ' mistakes.” - Mahatma Gandhi'
